739d50e3c5de9ac320f12b99b25ce360b006cce8
Buker, Nick <Nick.Buker@nordstrom.com>
2018-02-22 15:26:20 -0800

    add bars, colors and mem

--- pygame_monitor.py
+++ pygame_monitor.py
@@ -3,42 +3,72 @@ import psutil
 import pygame
 from pygame.locals import *
 
+# initialize pygame thingies
 pygame.init()
 pygame.font.init()
 
+# set some constants
 GRAY = (178, 178, 178)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 YELLOW = (232, 228, 0)
 ORANGE = (252, 151, 0)
 RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-
 FPS = 1
-
-DISPLAY = pygame.display.set_mode((320, 180))
 DEFAULT_FONT = pygame.font.get_default_font()
-MY_FONT = pygame.font.Font(DEFAULT_FONT, 16)
+
+# create some objects
+Display = pygame.display.set_mode((320, 180))
+MyFont = pygame.font.Font(DEFAULT_FONT, 16)
 pygame.display.set_caption('System Monitor')
 clock = pygame.time.Clock()
-
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    DISPLAY.fill(BLACK)
+    Display.fill(BLACK)
 
     cpu_perc = psutil.cpu_percent()
-    pygame.draw.rect(DISPLAY, GREEN, (10, 10, 3 * cpu_perc, 50))
-    cpu_text = MY_FONT.render('CPU usage:  {}%'.format(cpu_perc), True, GRAY)
-    DISPLAY.blit(cpu_text, (10, 70))
+    i = 0
+    for _ in range(20):
+        if 3 * cpu_perc > i :
+            width = 0
+        else:
+            width = 1
+        if i < 120:
+            color = GREEN
+        elif i < 180:
+            color = YELLOW
+        elif i < 240:
+            color = ORANGE
+        else:
+            color = RED
+        pygame.draw.rect(Display, color, (10 + i, 10, 12, 50), width)
+        i += 15
+    cpu_text = MyFont.render('CPU usage:  {}%'.format(cpu_perc), True, GRAY)
+    Display.blit(cpu_text, (10, 70))
 
     mem_perc = psutil.virtual_memory()[2]
-    pygame.draw.rect(DISPLAY, ORANGE, (10, 95, 3 * mem_perc, 50))
-    mem_text = MY_FONT.render('Memory usage:  {}%'.format(mem_perc), True, GRAY)
-    DISPLAY.blit(mem_text, (10, 155))
+    i = 0
+    for _ in range(20):
+        if 3 * mem_perc > i:
+            width = 0
+        else:
+            width = 1
+        if i < 120:
+            color = GREEN
+        elif i < 180:
+            color = YELLOW
+        elif i < 240:
+            color = ORANGE
+        else:
+            color = RED
+        pygame.draw.rect(Display, color, (10 + i, 95, 12, 50), width)
+        i += 15
+    mem_text = MyFont.render('Memory usage:  {}%'.format(mem_perc), True, GRAY)
+    Display.blit(mem_text, (10, 155))
 
     pygame.display.update()
     clock.tick(FPS)
